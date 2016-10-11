eeaa325cb2e371dadbacb29e48be2fe0e8d9993d
GiantMing <971334483@qq.com>
2016-10-11 17:20:18 +0800

    2.36

---
+++ 2.36.scm
@@ -0,0 +1,46 @@
+(define (accumulate op initial sequence)
+    (if (null? sequence) 
+        initial 
+        (op (car sequence)
+            (accumulate op initial (cdr sequence)))))
+
+
+(define (car-n seqs)
+    (map car seqs))
+
+(define (cdr-n seqs)
+  (map cdr seqs))
+
+(define s (list (list 1 2 3)
+                      (list 4 5 6)
+                      (list 7 8 9)
+                      (list 10 11 12)))
+
+(define (accumulate-n op init seqs)
+    (if (null? (car seqs))
+        '()
+        (cons (accumulate op init (car-n seqs))
+              (accumulate-n op init (cdr-n seqs)))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
